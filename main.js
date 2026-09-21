3a6699fa9df286e5eecffb68a471d20164bd2dbd
Elena Guerra <elena.guerra@catolica.edu.sv>
2026-09-21 16:34:55 -0600

    Tercer commit: agregue una funcion interactiva con JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-function encender(){
-    return alert("Hola mundo");
+function mostrarMensaje() {
+    alert("Este proyecto forma parte de una actividad de la materaia de tecnologias web, en la que aprendemos a utilizar Git y GitHub. En esta página se presentan sus conceptos principales, comandos básicos y la importancia del control de versiones.");
 }
